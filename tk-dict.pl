cd7560166f7c41329d0f14831966511ff91cef88
chenchacha <chenchacha@debian>
2015-10-03 15:52:43 +0800

    Split the text to note and dictionary.

--- tk-dict.pl
+++ tk-dict.pl
@@ -22,23 +22,43 @@ my $TOP = $MW->Frame;
 my $top_frame = $MW->Frame;
 my $bottom_frame = $MW->Frame;
 
-my $word_label = $top_frame->Label(-text => 'word')->pack(-expand => 0,
-							  -fill => 'x',
-							  -side => 'top');
+#Place the input entry
+my $word_label = $top_frame->Label(-text => 'word')
+    ->pack(-expand => 0,
+	   -fill => 'x',
+	   -side => 'top');
 
-my $word_entry = $top_frame->Scrolled('Entry')->pack(-expand => 0,
-						     -fill => 'x',
-						     -side => 'bottom');
+my $word_entry = $top_frame->Scrolled('Entry')
+    ->pack(-expand => 0,
+	   -fill => 'x',
+	   -side => 'bottom');
 $word_entry->configure(-textvariable => \$input);
 
+#Place the note text
+my $note_frame = $bottom_frame->Frame;
+my $frame_note_label = $note_frame->Label(-text => 'note')
+    ->pack(-expand => 0,
+	   -fill => 'x',
+	   -side => 'top');
 
-my $frame_label = $bottom_frame->Label(-text => 'Dictionary')->pack(-expand => 0,
-								    -fill => 'x',
-								    -side => 'top');
+my $frame_note_text = $note_frame->Scrolled('Text')
+    ->pack(-expand => 1,
+	   -fill => 'both',
+	   -side => 'bottom');
+$note_frame->pack(-expand => 1, -fill => 'both', -side => 'left');
 
-my $frame_text = $bottom_frame->Scrolled('Text')->pack(-expand => 1,
-						       -fill => 'both',
-						       -side => 'bottom');
+#Place the dictionary text
+my $dict_frame = $bottom_frame->Frame;
+my $frame_dict_label = $dict_frame->Label(-text => 'Dictionary')
+    ->pack(-expand => 0,
+	   -fill => 'x',
+	   -side => 'top');
+
+my $frame_dict_text = $dict_frame->Scrolled('Text')
+    ->pack(-expand => 1,
+	   -fill => 'both',
+	   -side => 'bottom');
+$dict_frame->pack(-expand => 1, -fill => 'both', -side => 'right');
 
 
 $top_frame->pack(-expand => 0, -fill => 'both', -side => 'top');
@@ -47,7 +67,10 @@ $bottom_frame->pack(-expand => 1, -fill => 'both', -side => 'bottom');
 ##
 # bind function
 #
-$word_entry->bind('<Return>' => [\&input_word, $frame_text, \$input]);
+$word_entry->bind('<Return>' => [\&input_word, 
+				 $frame_note_text, 
+				 $frame_dict_text, 
+				 \$input]);
 
 $MW->MainLoop;
 
@@ -58,48 +81,51 @@ sub input_word {
     # $entry - Reference to entry widget.
     # $text - Reference to text widget.
     # $word - input word.
-    my ($entry, $text, $word) = @_;
+    my ($entry, $note, $dict, $word) = @_;
 
     my @display;
-    $text->delete(qw/1.0 end/);
+    $note->delete(qw/1.0 end/);
+    find_note($$word, \@display);
+    while (my $key = shift @display) {
+        $note->insert('end', $key);
+    }
+
+    $dict->delete(qw/1.0 end/);
     find_dict($$word, \@display);
     while (my $key = shift @display) {
-        $text->insert('end', $key);
+        $dict->insert('end', $key);
     }
 }
 
-sub find_dict {
+sub find_note {
     my ($input, $display) = @_;
 
-#    print $$display "input:";
     chomp($input);
-
-    ## Input control
     # discard empty word
     if ($input !~ m/^[\w\.\-\"\p{Han}]+/) {
         next;
     }
 
-    print "$input" . "\n";
-    print "=========NOTE=========\n";
-    push @$display, "=========NOTE=========\n";
     open (OUTPUT, "./$CMD $NOTE $input |") or die "Can't open ./$CMD $NOTE $input: $!";
     while (my $line = <OUTPUT>) {
-        print "$line"; # add empty for complete double byte charecture.
+#        print "$line"; # add empty for complete double byte charecture.
 	push @$display, decode('utf-8', "$line");
     }
     close OUTPUT;
-    print "\n";
-    if ($input !~ m/ add /) {
-        print "=======DICTIONARY=====\n";
-	push @$display, "=======DICTIONARY=====\n";
-        open (OUTPUT, "./$CMD $DICT $input |") or die "Can't open ./$CMD $DICT $input: $!";
-        while (my $line = <OUTPUT>) {
-            print "$line"; # add empty for complete double byte charecture.
-	    push @$display, decode('utf-8', "$line");
-        }
-        close OUTPUT;
-        print "\n";
+}
+
+sub find_dict {
+    my ($input, $display) = @_;
+
+    chomp($input);
+    # discard empty word
+    if ($input !~ m/^[\w\.\-\"\p{Han}]+/) {
+        next;
     }
-    print "======================\n";
+
+    open (OUTPUT, "./$CMD $DICT $input |") or die "Can't open ./$CMD $DICT $input: $!";
+    while (my $line = <OUTPUT>) {
+	push @$display, decode('utf-8', "$line");
+    }
+    close OUTPUT;
 }
